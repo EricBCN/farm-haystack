be1a315394a3ef054899364e44a6aa95216395ca
PiffPaffM <markuspaff.mp@gmail.com>
2021-03-03 19:26:06 +0100

    first easy implementation of feedback

--- ui/utils.py
+++ ui/utils.py
@@ -5,6 +5,7 @@ import os
 API_ENDPOINT = os.getenv("API_ENDPOINT", "http://localhost:8000")
 MODEL_ID = "1"
 DOC_REQUEST = "doc-qa"
+DOC_FEEDBACK = "doc-qa-feedback"
 
 def format_request(question,filters=None,top_k_reader=5,top_k_retriever=5):
     if filters == None:
@@ -38,5 +39,22 @@ def retrieve_doc(question,filters=None,top_k_reader=5,top_k_retriever=5):
            context = '...' + answers[i]['context'] + '...'
            meta_name = answers[i]['meta']['name']
            relevance = round(answers[i]['probability']*100,2)
-           result.append({'context':context,'answer':answer,'source':meta_name,'relevance':relevance})
+           document_id = answers[i]['document_id']
+           offset_start_in_doc = answers[i]['offset_start_in_doc']
+           result.append({'context':context,'answer':answer,'source':meta_name,'relevance':relevance,'document_id':document_id,'offset_start_in_doc':offset_start_in_doc})
    return result, response_raw
+
+def feedback_doc(question,is_correct_answer,document_id,model_id,is_correct_document,answer,offset_start_in_doc):
+   # Query Haystack API
+   url = API_ENDPOINT + '/' + DOC_FEEDBACK
+   req = {
+         "question": question,
+         "is_correct_answer": is_correct_answer,
+         "document_id":  document_id,
+         "model_id": model_id,
+         "is_correct_document": is_correct_document,
+         "answer": answer,
+         "offset_start_in_doc": offset_start_in_doc
+         }
+   response_raw = requests.post(url,json=req).json()
+   return response_raw
--- ui/webapp.py
+++ ui/webapp.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from utils import retrieve_doc
+from utils import feedback_doc
 from annotated_text import annotated_text
+import st_state_patch
 
 def annotate_answer(answer,context):
     start_idx = context.find(answer)
@@ -12,8 +14,16 @@ st.sidebar.header("Options")
 top_k_reader = st.sidebar.slider("Max. number of answers",min_value=1,max_value=10,value=3,step=1)
 top_k_retriever = st.sidebar.slider("Max. number of documents from retriever",min_value=1,max_value=10,value=3,step=1)
 question = st.text_input("Please provide your query:",value="Who is the father of Arya Starck?")
-run_query = st.button("Run")
+s = st.State()
+if s and s.run_query:
+    run_query = s.run_query
+    st.button("Run")
+else:
+    run_query = st.button("Run")
+s.run_query = run_query
+
 debug = st.sidebar.checkbox("Show debug info")
+raw_json_feedback = ""
 if run_query:
     with st.spinner("Performing neural search on documents... 🧠 \n "
                     "Do you want to optimize speed or accuracy? \n"
@@ -23,6 +33,18 @@ if run_query:
     for result in results:
         annotate_answer(result['answer'],result['context'])
         '**Relevance:** ', result['relevance'] , '**Source:** ' , result['source']
+        if st.button("Correct answer", key=result['answer']):
+           st.write("Correct answer")
+           raw_json_feedback = feedback_doc(question,"true",result['document_id'],1,"true",result['answer'],result['offset_start_in_doc'])
+        if st.button("Wrong answer and wrong passage", key=result['answer']):
+           st.write("Wrong answer and wrong passage")
+           raw_json_feedback = feedback_doc(question,"false",result['document_id'],1,"false",result['answer'],result['offset_start_in_doc'])
+        if st.button("wrong answer, but correct passage", key=result['answer']):
+           st.write("wrong answer, but correct passage")
+           raw_json_feedback = feedback_doc(question,"false",result['document_id'],1,"true",result['answer'],result['offset_start_in_doc'])
     if debug:
         st.subheader('REST API JSON response')
         st.write(raw_json)
+if debug:
+   st.write(raw_json_feedback)
+   
